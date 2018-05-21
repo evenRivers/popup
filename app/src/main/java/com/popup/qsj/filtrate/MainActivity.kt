51e2e95b4ea9c342c1a66261431efa6ea23b89b4
qiusj <972045606@qq.com>
2018-05-21 16:12:32 +0800

    qiusj
    初始修改

--- app/src/main/java/com/popup/qsj/filtrate/MainActivity.kt
+++ app/src/main/java/com/popup/qsj/filtrate/MainActivity.kt
@@ -2,11 +2,20 @@ package com.popup.qsj.filtrate
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+    private fun init() {
+        bt.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this,"1111",Toast.LENGTH_SHORT).show();
+        })
     }
 }
