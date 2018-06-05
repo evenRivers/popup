e883b677492f358d800d210dbf495cf4f8d3b530
qiusj <972045606@qq.com>
2018-06-05 15:13:42 +0800

    qiusj
    数据源

--- app/src/main/java/com/popup/qsj/filtrate/MainActivity.kt
+++ app/src/main/java/com/popup/qsj/filtrate/MainActivity.kt
@@ -11,6 +11,7 @@ import popup.bean.M_Data
 
 class MainActivity : AppCompatActivity() {
     private  lateinit var mContext:Context
+    private var mList= arrayListOf<M_Bean>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +20,15 @@ class MainActivity : AppCompatActivity() {
     }
     private fun init() {
         mContext = this
+        initData()
+
+        bt.setOnClickListener(View.OnClickListener {
+//            Toast.makeText(this,"显示筛选框",Toast.LENGTH_SHORT).show();
+            FilterPop(mContext,mList,bt)
+        })
+    }
+
+    fun initData(){
         val bean = M_Bean()
         bean.title = "编程"
         val dataList = arrayListOf<M_Data>()
@@ -58,16 +68,35 @@ class MainActivity : AppCompatActivity() {
         dataList.add(data7)
         dataList.add(data8)
         dataList.add(data9)
-
         bean.list = dataList
-
-        var mList= arrayListOf<M_Bean>()
         mList.add(bean)
+        //第二部分
+        val bean1 = M_Bean()
+        bean1.title = "手机"
+        val dataList1 = arrayListOf<M_Data>()
+        val data11 = M_Data()
+        data11.id = 11;
+        data11.name = "Android"
+        val data12 = M_Data()
+        data12.id = 12;
+        data12.name = "IOS"
+        val data13 = M_Data()
+        data13.id = 3;
+        data13.name = "WP"
+        val data14 = M_Data()
+        data14.id = 4;
+        data14.name = "HTML5"
+        val data15 = M_Data()
+        data15.id = 5;
+        data15.name = "VUE"
+        dataList1.add(data11)
+        dataList1.add(data12)
+        dataList1.add(data13)
+        dataList1.add(data14)
+        dataList1.add(data15)
+        bean1.list = dataList1
+        mList.add(bean1)
 
-        bt.setOnClickListener(View.OnClickListener {
-//            Toast.makeText(this,"显示筛选框",Toast.LENGTH_SHORT).show();
-            FilterPop(mContext,mList,bt)
-        })
     }
 
 }
